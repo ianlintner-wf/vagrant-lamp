30090cf26c57d604576606468cc6b008702c268f
Ian.Lintner <Ian.Lintner@WebFilings.com>
2014-03-24 00:51:16 -0500

    Up php memory limit in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,5 @@ if [[ ! -e $dir ]]; then
 elif [[ ! -d $dir ]]; then
     echo "$dir already exists but is not a directory" 1>&2
 fi
+
+sed -i 's/memory_limit = .*/memory_limit = 512M/' /etc/php5/apache2/php.ini
