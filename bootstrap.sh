7d43c17591275fd8ebbc5491ded82d9b15432074
Ian.Lintner <Ian.Lintner@WebFilings.com>
2014-03-24 01:05:24 -0500

    UPDATED bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-dir=/mnt/webdata/ 
+dir=/mnt/webdata/www_files
 if [[ ! -e $dir ]]; then
     mkdir -p $dir
     chmod 777 $dir -R
@@ -7,4 +7,15 @@ elif [[ ! -d $dir ]]; then
     echo "$dir already exists but is not a directory" 1>&2
 fi
 
+dir=/mnt/webdata/www_tmp
+if [[ ! -e $dir ]]; then
+    mkdir -p $dir
+    chmod 777 $dir -R
+elif [[ ! -d $dir ]]; then
+    echo "$dir already exists but is not a directory" 1>&2
+fi
+
+
 sed -i 's/memory_limit = .*/memory_limit = 512M/' /etc/php5/apache2/php.ini
+
+service apache2 reload
